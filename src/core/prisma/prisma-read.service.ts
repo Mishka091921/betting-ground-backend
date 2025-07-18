c926bc8339cd901132da7ecb688cb5e0bac4ba43
joshua <joshuagementiza.groundlin>
2025-07-18 14:21:11 +0800

    fix: write and read prisma service

--- src/core/prisma/prisma-read.service.ts
+++ src/core/prisma/prisma-read.service.ts
@@ -1,10 +1,20 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaReadService extends PrismaClient {
+export class PrismaReadService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
     const url = `mysql://${process.env.SQL_READ_USERNAME}:${process.env.SQL_READ_PASSWORD}@${process.env.SQL_READ_HOST}:${process.env.SQL_READ_PORT}/${process.env.SQL_READ_DB}`;
     super({ datasources: { db: { url } } });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('[PrismaReadService] Connected to READ database');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('[PrismaReadService] Disconnected from READ database');
   }
 }
--- src/core/prisma/prisma-write.service.ts
+++ src/core/prisma/prisma-write.service.ts
@@ -1,10 +1,20 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaWriteService extends PrismaClient {
+export class PrismaWriteService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
     const url = `mysql://${process.env.SQL_WRITE_USERNAME}:${process.env.SQL_WRITE_PASSWORD}@${process.env.SQL_WRITE_HOST}:${process.env.SQL_WRITE_PORT}/${process.env.SQL_WRITE_DB}`;
     super({ datasources: { db: { url } } });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('[PrismaWriteService] Connected to WRITE database');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('[PrismaWriteService] Disconnected from WRITE database');
   }
 }
